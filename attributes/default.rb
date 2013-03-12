93dae9b43860f05153004acf0d9b8909c54dac9b
interu <beautybeast07@gmail.com>
2013-03-12 19:17:49 +0900

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,18 @@
+# #-*- encoding : utf-8 -*-
+# === VERSION AND LOCATION
+#
+default.elasticsearch[:version]       = "0.20.5"
+default.elasticsearch[:host]          = "http://download.elasticsearch.org"
+default.elasticsearch[:repository]    = "elasticsearch/elasticsearch"
+default.elasticsearch[:filename]      = "elasticsearch-#{node.elasticsearch[:version]}.tar.gz"
+default.elasticsearch[:download_url]  = [node.elasticsearch[:host], node.elasticsearch[:repository], node.elasticsearch[:filename]].join('/')
+
+# === USER & PATHS
+#
+default.elasticsearch[:dir]       = "/usr/local"
+default.elasticsearch[:user]      = "elasticsearch"
+default.elasticsearch[:home_dir]  = [node.elasticsearch[:dir], node.elasticsearch[:user]].join('/')
+
+default.elasticsearch[:path][:conf] = [node.elasticsearch[:home_dir], "config"].join('/')
+default.elasticsearch[:path][:data] = [node.elasticsearch[:home_dir], "data"].join('/')
+default.elasticsearch[:path][:logs] = [node.elasticsearch[:home_dir], "logs"].join('/')
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+#-*- encoding : utf-8 -*-
+maintainer       "Teruo Adachi"
+maintainer_email "interu@sonicgarden.jp"
+license          "Apache"
+description      "Installs elasticsearch"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+depends "java"
