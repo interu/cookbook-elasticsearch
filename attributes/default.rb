fde392a17fc739be350c021057186ea6959c597b
interu <beautybeast07@gmail.com>
2013-03-26 15:19:03 +0900

    elasticsearchの基本設定を追加

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,5 @@
 # #-*- encoding : utf-8 -*-
 # === VERSION AND LOCATION
-#
 default.elasticsearch[:version]       = "0.20.5"
 default.elasticsearch[:host]          = "http://download.elasticsearch.org"
 default.elasticsearch[:repository]    = "elasticsearch/elasticsearch"
@@ -8,13 +7,13 @@ default.elasticsearch[:filename]      = "elasticsearch-#{node.elasticsearch[:ver
 default.elasticsearch[:download_url]  = [node.elasticsearch[:host], node.elasticsearch[:repository], node.elasticsearch[:filename]].join('/')
 
 # === USER & PATHS
-#
-default.elasticsearch[:dir]       = "/usr/local"
+default.elasticsearch[:dir]       = "/opt"
 default.elasticsearch[:user]      = "elasticsearch"
 default.elasticsearch[:home_dir]  = [node.elasticsearch[:dir], node.elasticsearch[:user]].join('/')
 
 default.elasticsearch[:path][:conf] = [node.elasticsearch[:home_dir], "config"].join('/')
 default.elasticsearch[:path][:data] = [node.elasticsearch[:home_dir], "data"].join('/')
-default.elasticsearch[:path][:logs] = [node.elasticsearch[:home_dir], "logs"].join('/')
-default.elasticsearch[:path][:pids]  = '/var/run/elasticsearch'
+default.elasticsearch[:path][:plugins] = [node.elasticsearch[:home_dir], "plugins"].join('/')
+default.elasticsearch[:path][:logs] = ['/var/log',node.elasticsearch[:user]].join('/')
+default.elasticsearch[:path][:pids] = '/var/run'
 default.elasticsearch[:pid_file]  = [node.elasticsearch[:path][:pids], "elasticsearch.pid"].join('/')
