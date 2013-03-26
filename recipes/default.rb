7d43758aed5a9579742d5e20c9803b5dc8936401
interu <beautybeast07@gmail.com>
2013-03-27 00:37:14 +0900

    separate install module

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,47 +1,4 @@
 #-*- encoding : utf-8 -*-
-elasticsearch = "elasticsearch-#{node.elasticsearch[:version]}"
-include_recipe "elasticsearch::packages"
-
-# Create user and group
-group node.elasticsearch[:user] do
-  action :create
-end
-
-user node.elasticsearch[:user] do
-  comment "ElasticSearch User"
-  home    "/home/elasticsearch"
-  shell   "/bin/bash"
-  gid     node.elasticsearch[:user]
-  supports :manage_home => false
-  action  :create
-end
-
-# Install ElasticSearch
-script "install_elasticsearch" do
-  interpreter "bash"
-  user "root"
-  cwd "/tmp"
-  code <<-EOH
-    wget "#{node.elasticsearch[:download_url]}"
-    tar xvzf #{node.elasticsearch[:filename]} -C #{node.elasticsearch[:dir]}
-    ln -s "#{node.elasticsearch[:home_dir]}-#{node.elasticsearch[:version]}" #{node.elasticsearch[:home_dir]}
-  EOH
-end
-
-# Create Symlink
-  link "#{node.elasticsearch[:home_dir]}" do
-  to "#{node.elasticsearch[:home_dir]}-#{node.elasticsearch[:version]}"
-end
-
-# Install Kuromoji plugin
-script "install_plugins" do
-  interpreter "bash"
-  user "root"
-  cwd "#{node.elasticsearch[:home_dir]}"
-  code <<-EOH
-    bin/plugin -install elasticsearch/elasticsearch-analysis-kuromoji/1.1.0
-  EOH
-end
 
 # Create Directories
 [ node.elasticsearch[:path][:conf], node.elasticsearch[:path][:data], node.elasticsearch[:path][:logs], node.elasticsearch[:path][:pids] ].each do |path|
@@ -80,7 +37,6 @@ service "elasticsearch" do
   supports :status => true, :restart => true
   action [ :enable ]
 end
-
 
 # Monitoring by Monit
 template "elasticsearch.monitrc" do
